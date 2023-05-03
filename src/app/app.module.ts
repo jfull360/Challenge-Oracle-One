c3a84c3120129f2628cbee007fa9274753364d93
jdominguez <76186137+jfull360@users.noreply.github.com>
2023-05-03 17:48:35 -0600

    ADD show-result
    :+1:  Se creo el componente show-result para mostrar el resultado del compilador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Compiladorcomponent } from './components/compilador/compilador.component';
+import { ShowResultComponent } from './components/show-result/show-result.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Compiladorcomponent
+    Compiladorcomponent,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
