53167f4f63378e1a12a60d593f3984b06a1facb8
JORGE FULGENCIO DOMINGUEZ JAEN <jfull360@gmail.com>
2023-05-07 18:01:39 -0600

    ADD historial component / PipeTexto Pipe / CHANGE styles
    :+1:  : Se genero un componente llamado historial funcional que servirá para mostrar los mensajes encriptados anteriormente por el usuario, se le agregaron validaciones ademas de un Pipe para evitar deformar el template
    
    :+1:  : Cambios y correcciones en los estilos

---
+++ src/app/components/historial/historial.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Pipe,
+  PipeTransform
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { objetoHistorial } from '../compilador/compilador.component';
+@Pipe({
+  name: 'pipeTexto',
+  standalone: true
+})
+export class PipeTexto implements PipeTransform {
+  transform(texto: string): string {
+    // si el texto sobrepasa los 10 caracteres solo presentaremos esos 10 caracteres
+    //y evitar deformar el template
+    if (texto.length >= 10) {
+      return texto.substring(0, 9) + ' ...';
+    } else {
+      return texto;
+    }
+  }
+}
+@Component({
+  selector: 'historial',
+  templateUrl: './historial.component.html',
+  styleUrls: ['./historial.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule, BrowserModule, PipeTexto]
+})
+export class HistorialComponent implements OnInit {
+  @Input() historial: objetoHistorial[] = [];
+  @Output() textoEvent = new EventEmitter<string>();
+
+  constructor(private clipboard: Clipboard) {}
+
+  ngOnInit() {}
+  enviarMensaje(texto: string) {
+    this.textoEvent.emit(texto);
+    console.log(texto);
+  }
+}
